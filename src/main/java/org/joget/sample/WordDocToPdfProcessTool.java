4046dbb5ad683ea5fd6042dfd0512b1f5342f66b
aqilahani-joget <nor.aqilahani@joget.com>
2025-01-14 10:50:55 +0800

    working version

--- src/main/java/org/joget/sample/WordDocToPdfProcessTool.java
+++ src/main/java/org/joget/sample/WordDocToPdfProcessTool.java
@@ -1,14 +1,20 @@
 package org.joget.sample;
 
-import org.docx4j.Docx4J;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import fr.opensagres.poi.xwpf.converter.core.XWPFConverterException;
+
+import java.util.HashMap;
 import java.util.Map;
-import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import java.io.IOException;
 import org.joget.apps.app.dao.FormDefinitionDao;
+import org.joget.apps.app.lib.DatabaseUpdateTool;
 import org.joget.apps.app.model.AppDefinition;
 import org.joget.apps.app.model.FormDefinition;
 import org.joget.apps.app.service.AppPluginUtil;
@@ -75,7 +81,8 @@ public class WordDocToPdfProcessTool extends DefaultApplicationPlugin {
         String id = appService.getOriginProcessId(wfAssignment.getProcessId());
 
         LogUtil.info(getClassName(), "recordId: " + id);
-        String filenameColumn = (String) properties.get("filenameColumn");
+        String docFilenameColumn = (String) properties.get("docFilenameColumn");
+        String pdfFilenameColumn = (String) properties.get("pdfFilenameColumn");
         
         //Load the original Form Data record
         AppDefinition appDef = (AppDefinition) properties.get("appDef");
@@ -91,7 +98,7 @@ public class WordDocToPdfProcessTool extends DefaultApplicationPlugin {
         String path = FileUtil.getUploadPath(formDef.getTableName(), id);
         
         LogUtil.info(getClassName(), "path: " + path);
-        String docxFilename = row.getProperty(filenameColumn);
+        String docxFilename = row.getProperty(docFilenameColumn);
         
         LogUtil.info(getClassName(), "filename: " + docxFilename);
         
@@ -101,18 +108,28 @@ public class WordDocToPdfProcessTool extends DefaultApplicationPlugin {
             return null;
         }
         String pdfFilename = docxFilename.substring(0, docxFilename.lastIndexOf(".")) + ".pdf";
-        try {
-            File file = new File((path + docxFilename));
-            if (file.exists()) {
-                WordprocessingMLPackage wordMLPackage = Docx4J.load(file);
-                OutputStream os = new FileOutputStream(new File(path + pdfFilename));
-                Docx4J.toPDF(wordMLPackage, os);
-            }
-            LogUtil.info(getClassName(), "File does not exist");
+        try (InputStream docFile = new FileInputStream(new File(path + docxFilename));
+             OutputStream out = new FileOutputStream(new File(path + pdfFilename))) {
             
-        } catch (Docx4JException | FileNotFoundException ex) {
-            LogUtil.error(getClassName(), ex, "");
+            XWPFDocument doc = new XWPFDocument(docFile);
+            PdfOptions pdfOptions = PdfOptions.create();
+            PdfConverter.getInstance().convert(doc, out, pdfOptions);
+            
+        } catch (XWPFConverterException | IOException ex) {
+            LogUtil.error(getClassName(), ex, "Error converting DOCX to PDF: " + ex.getMessage());
         }
+
+        //update pdf file column
+        updatePdfField(wfAssignment, id, pdfFilenameColumn, pdfFilename, formDef.getTableName());
+
         return null;
+    }
+
+    private void updatePdfField(WorkflowAssignment wfAssignment, String recordId, String pdfColumn, String pdfFilename, String tableName) {        
+        Map propertiesMap = new HashMap();
+        propertiesMap.put("workflowAssignment", wfAssignment);
+        propertiesMap.put("jdbcDatasource", "default");
+        propertiesMap.put("query", "UPDATE app_fd_" + tableName + " SET c_" + pdfColumn + " = '" + pdfFilename + "' WHERE id = '" + recordId + "'");
+        new DatabaseUpdateTool().execute(propertiesMap);
     }
 }
